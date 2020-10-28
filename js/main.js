fc94edd8c3303eb0b32e190853493da8b71d15de
Elizaveta O <souslayk@gmail.com>
2020-10-28 19:29:10 +0300

    file

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
-$('.video').parent().click(function () {
-
-  if ($(this).children(".video").get(0).paused) {
-    $(this).children(".video").get(0).play();
-    $(this).children(".playpause").fadeOut();
-
-  } else {
-    $(this).children(".video").get(0).pause();
-
-    $(this).children(".playpause").fadeIn();
-
-  }
-
-});
-
-
 $('.burger').click(function(event) {
-  $('.burger, .nav').toggleClass('active');
-  $('body').toggleClass('.lock');
+  $('.burger, .navigationMenu').toggleClass('active');
+  //$('body').toggleClass('.lock');
 });
